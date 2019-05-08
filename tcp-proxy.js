db7e6abf97e79a7171f02e0cd9f574b79eee3573
汪浩 <wanghao@ithinkdt.com>
2019-05-08 13:14:21 +0800

    add hashByIP support

--- tcp-proxy.js
+++ tcp-proxy.js
@@ -22,6 +22,14 @@ function parse(o) {
     }
 }
 
+/**
+ * 启动服务
+ * @param proxyPort
+ * @param serviceHost
+ * @param servicePort
+ * @param options
+ * @constructor
+ */
 function TcpProxy(proxyPort, serviceHost, servicePort, options) {
     this.proxyPort = proxyPort;
     this.serviceHosts = parse(serviceHost);
@@ -49,24 +57,34 @@ function TcpProxy(proxyPort, serviceHost, servicePort, options) {
     this.createListener();
 }
 
+/**
+ * 创建代理监听
+ */
 TcpProxy.prototype.createListener = function() {
     var self = this;
     if (self.options.tls !== false) {
+        console.log("#TLS");
         self.server = tls.createServer(self.proxyTlsOptions, function(socket) {
-            self.handleClient(socket);
+          // socket.setEncoding("hex");
+          self.handleClient(socket);
         });
     } else {
+      console.log("#NO TLS");
         self.server = net.createServer(function(socket) {
+          socket.setEncoding("hex");
             self.handleClient(socket);
         });
     }
     self.server.listen(self.proxyPort, self.options.hostname);
 };
 
+/**
+ * 处理客户端请求
+ * @param proxySocket
+ */
 TcpProxy.prototype.handleClient = function(proxySocket) {
     var self = this;
     var key = uniqueKey(proxySocket);
-    console.log(`#key:${key}`);
     self.proxySockets[key] = proxySocket;
     var context = {
         key: key,
@@ -77,63 +95,86 @@ TcpProxy.prototype.handleClient = function(proxySocket) {
     self.createServiceSocket(context);
     proxySocket.on("data", function(data) {
         if (context.connected) {
-            context.serviceSocket.write(data);
+            console.log("#recive:" + data);
+            context.serviceSocket.write(data, "hex");
         } else {
             context.buffers[context.buffers.length] = data;
         }
     });
     proxySocket.on("close", function(hadError) {
+      console.log("#Close:" + hadError);
         delete self.proxySockets[uniqueKey(proxySocket)];
         context.serviceSocket.destroy();
     });
     proxySocket.on("error", function(e) {
+      console.error(e);
         context.serviceSocket.destroy();
     });
 };
 
+/**
+ * 创建转发服务
+ * @param context
+ */
 TcpProxy.prototype.createServiceSocket = function(context) {
     var self = this;
     var i = self.getServiceHostIndex(context.key);
-    console.log(`Idx:${i}`);
     if (self.options.tls === "both") {
+      console.log("#TLS CONN");
         context.serviceSocket = tls.connect(self.servicePorts[i],
             self.serviceHosts[i], self.serviceTlsOptions, function() {
                 self.writeBuffer(context);
             });
+
     } else {
+      console.log("#NO TLS CONN");
         context.serviceSocket = new net.Socket();
+      context.serviceSocket.setEncoding("hex");
         context.serviceSocket.connect(self.servicePorts[i],
             self.serviceHosts[i], function() {
                 self.writeBuffer(context);
             });
     }
     context.serviceSocket.on("data", function(data) {
-        context.proxySocket.write(data);
+      console.log("#Response:" + data);
+        context.proxySocket.write(data, "hex");
     });
     context.serviceSocket.on("close", function(hadError) {
+      console.error("#Targe Server Close:" + hadError);
         context.proxySocket.destroy();
     });
     context.serviceSocket.on("error", function(e) {
+      console.error("#Targe Server Error:" + e);
         context.proxySocket.destroy();
     });
 };
 
+/**
+ * 获取服务器序号
+ * @param key
+ * @returns {*|number}
+ */
 TcpProxy.prototype.getServiceHostIndex = function(key) {
     var self = this;
     var idx = self.getHashedHostIndex(key);
     if(idx != -1) {
+        console.log(`Proxy: ${key} to ${self.serviceHosts[this.serviceHostIndex]}:${self.servicePorts[this.serviceHostIndex]} from Hash`);
         return idx;
     } else {
       this.serviceHostIndex++;
       if (this.serviceHostIndex == this.serviceHosts.length) {
         this.serviceHostIndex = 0;
-        this.releaseTimeoutHash();
       }
+      this.releaseTimeoutHash();
       this.setHashedHostIndex(key, this.serviceHostIndex);
+      console.log(`Proxy: ${key} to ${self.serviceHosts[this.serviceHostIndex]}:${self.servicePorts[this.serviceHostIndex]}`);
       return this.serviceHostIndex;
     }
 };
 
+/**
+ * 释放HASH
+ */
 TcpProxy.prototype.releaseTimeoutHash = function() {
     var time = Date.now();
     for(var key in this.hashedServer) {
@@ -146,17 +187,13 @@ TcpProxy.prototype.releaseTimeoutHash = function() {
 
 TcpProxy.prototype.setHashedHostIndex = function(key, idx) {
   console.log(`#Hash ${key} on Server ${idx}`);
-  // TODO for TEST only
-  if(key != '::ffff:10.110.40.81') {
-    this.hashedServer[key] = {i: idx, t: Date.now()};
-  }
+  this.hashedServer[key] = {i: idx, t: Date.now()};
 }
 
 TcpProxy.prototype.getHashedHostIndex = function(key) {
     var server = this.hashedServer[key];
     if(server != null) {
       server.t = Date.now();
-      console.log(`#Found Server:${server.t}`);
       return server.i;
     }
     return -1;
@@ -166,7 +203,8 @@ TcpProxy.prototype.writeBuffer = function(context) {
     context.connected = true;
     if (context.buffers.length > 0) {
         for (var i = 0; i < context.buffers.length; i++) {
-            context.serviceSocket.write(context.buffers[i]);
+          console.log("#write:" + context.buffers[i]);
+            context.serviceSocket.write(context.buffers[i], "hex");
         }
     }
 };
